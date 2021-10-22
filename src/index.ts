6f1b51e053dca430932ea5de52ad388d7e3618ae
memanot <menemty@gmail.com>
2021-10-22 10:57:32 +0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-type TDefSettings = {}
-
-class Def {
-  private _settings: TDefSettings
-
-  constructor(s: TDefSettings) {
-    this._settings = { ...s }
-  }
-}
-
-export { Def }
+export * from './types'
