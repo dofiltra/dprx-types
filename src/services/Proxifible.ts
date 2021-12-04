7cf3b2d7b1e5ac88fb511e5afb54571e28beba9b
Dmitriy <pointsman1@gmail.com>
2021-12-04 08:24:46 +0200

    proxifible: useCount zero after changeIp

--- src/services/Proxifible.ts
+++ src/services/Proxifible.ts
@@ -22,7 +22,7 @@ export class Proxifible {
       await this.incProxy(selectedProxy.url())
       if (selectedProxy.changeUrl) {
         if ((selectedProxy.useCount || 0) >= this.limitPerProxy) {
-          await this.changeProxyIp(selectedProxy.changeUrl)
+          await this.changeProxyIp(selectedProxy.changeUrl, selectedProxy.url())
         }
       }
       return selectedProxy
@@ -42,7 +42,7 @@ export class Proxifible {
     }
   }
 
-  static async changeProxyIp(url: string) {
+  static async changeProxyIp(url: string, proxyUrl: string) {
     try {
       if (!url.startsWith('http')) {
         url = `http://${url}`
@@ -56,6 +56,11 @@ export class Proxifible {
         method: 'GET',
         timeout: 60e3
       })
+
+      const index = this.proxies.findIndex((p) => p.url() === proxyUrl)
+      if (index > -1) {
+        this.proxies[index]!.useCount = 0
+      }
 
       return { result: await resp.json() }
     } catch (error: any) {
