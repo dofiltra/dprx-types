e1a063976a204ed3582991c98df487d0f7cea0a1
Dmitriy <pointsman1@gmail.com>
2021-12-04 10:24:45 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 /* tslint:disable:max-classes-per-file */
+import _ from 'lodash'
+import fetchHap, { FetchOptions } from 'make-fetch-happen'
 
 export type TAppDprxSettings = {
   serverName?: string
@@ -362,5 +364,91 @@ export type TBatchBlock = {
   rewritedCharsCount: number
 }
 
-export * from './fetcher'
-export * from './services/Proxifible'
+export type TFilterProxyOpts = {
+  filterTypes?: ('http' | 'https' | 'socks5')[]
+  sortBy?: ('useCount' | 'changeUrl')[]
+}
+
+export class Proxifible {
+  static proxies: ProxyItem[] = []
+  static limitPerProxy = 1000
+
+  static async getProxy(opts?: TFilterProxyOpts) {
+    const { filterTypes, sortBy = ['useCount'] } = { ...opts }
+    const sortProxies = _.sortBy(Proxifible.proxies, sortBy).filter(
+      (p) => !filterTypes?.length || filterTypes.includes(p.type)
+    )
+
+    const selectedProxy = sortProxies[0]
+    if (selectedProxy) {
+      await this.changeUseCountProxy(selectedProxy.url())
+      if (selectedProxy.changeUrl) {
+        if ((selectedProxy.useCount || 0) >= this.limitPerProxy) {
+          await this.changeIp(selectedProxy.changeUrl, selectedProxy.url())
+        }
+      }
+      return selectedProxy
+    }
+
+    return
+  }
+
+  static async changeUseCountProxy(proxyUrl?: string, inc = 1) {
+    if (!proxyUrl) {
+      return
+    }
+
+    try {
+      const index = this.proxies.findIndex((p) => p.url() === proxyUrl)
+      if (index > -1) {
+        if (inc === -this.limitPerProxy) {
+          this.proxies[index].useCount = 0
+        } else {
+          this.proxies[index]!.useCount = (this.proxies[index]!.useCount || 0) + inc
+        }
+      }
+    } catch (error: any) {
+      //
+    }
+  }
+
+  static async changeIp(url: string, proxyUrl: string) {
+    try {
+      if (!url.startsWith('http')) {
+        url = `http://${url}`
+      }
+      const fh = await getFetchHap()
+      const resp = await fh(url, {
+        headers: {
+          'cache-control': 'no-cache',
+          'content-type': 'application/json'
+        },
+        method: 'GET',
+        timeout: 60e3
+      })
+      this.changeUseCountProxy(proxyUrl, -this.limitPerProxy)
+
+      return { result: await resp.json() }
+    } catch (error: any) {
+      return { error }
+    }
+  }
+}
+
+export const getFetchHap = async (opts?: FetchOptions) => {
+  const {
+    headers = {
+      'User-Agent':
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3'
+    },
+    compress = true
+  } = { ...opts }
+
+  return fetchHap.defaults({
+    // cachePath: './node_modules/.fetch-cache',
+    timeout: 30e3,
+    ...opts,
+    compress,
+    headers
+  })
+}
